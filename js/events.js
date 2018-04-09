4fed0f1f8f8f5e360dbddfd15f7308a83f72c7b0
Daniel Doughty <djdoughty@gmail.com>
2018-04-09 21:04:03 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,4 +1,35 @@
 //define functions here
+function getIt () {
+  $(document).on('click', function() {
+    alert('Hey!')
+    return
+  })
+}
+
+function frameIt () {
+  $('img').on('load', function() {
+    $("img").addClass("tasty")
+    return
+  })
+}
+
+function pressIt () {
+  $(document).on('keydown', function() {
+    if ($("input:first").val() == 71) {
+    alert ('you have pressed the G key')  
+    }
+    return
+  })
+}  
+
+function submitIt () {
+  $('form').on('submit', function() {
+    
+    alert ('Your form is going to be submitted now.')  
+    
+    return
+  })
+}  
 
 $(document).ready(function(){
 
